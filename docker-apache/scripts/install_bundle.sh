0c18a40d96e645d95f42becda9a0c632b4045153
fusengine <info@fusengine.ch>
2015-12-28 23:19:31 +0100

    update

---
+++ docker-apache/scripts/install_bundle.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# add script function
+source /root/script_base.sh
+
+# add packages
+PACKAGES_DEFAULT="imagemagick apache2 php7.0 libapache2-mod-php7.0 libphp7.0-embed php7.0-dbg \
+                   php7.0-curl php7.0-gd php7.0-json php7.0-imap php7.0-mysql php7.0-sqlite3 \
+                   php7.0-modules-source php7.0-phpdbg php7.0-fpm php7.0-dev php7.0-common php-all-dev \
+                   php7.0-mcrypt ssmtp \
+
+                   xfonts-utils xfonts-base xfonts-75dpi \
+                   fontconfig libxrender1
+                   "
+
+# Udapte ubuntu
+install_packages
+
+# Configuration Apache part 2
+rm -rf /var/lib/apt/lists/* && \
+a2enmod php7.0 && \
+a2enmod rewrite && \
+service apache2 stop
+
+
+# If you want need Blackfire
+# RUN curl -A "Docker" -o /tmp/blackfire-probe.tar.gz -D - -L -s https://blackfire.io/api/v1/releases/probe/php/linux/amd64/55\
+#    && tar zxpf /tmp/blackfire-probe.tar.gz -C /tmp \
+#    && mv /tmp/blackfire-*.so `php -r "echo ini_get('extension_dir');"`/blackfire.so \
+#    && echo "extension=blackfire.so\nblackfire.agent_socket=\${BLACKFIRE_PORT}" > /etc/php5/apache2/conf.d/blackfire.ini
+
+
+# Configuration Apache part 2
+ echo "ServerName localhost" >> /etc/apache2/apache2.conf && \
+      sed -i "s/variables_order.*/variables_order = \"EGPCS\"/g" /etc/php/7.0/apache2/php.ini && \
+      sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
+
+
+# install wkhtmltopdf
+wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb && \
+     dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb && \
+     rm wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+
+  # Upgrade ubuntu
+  upgrade
+
+  # Clean ubuntu
+  clean_ubuntu
+
+# Create /app repertory and delete  /var/www/html
+mkdir -p /app/web && rm -fr /var/www/html
+chown www-data:www-data /app/web
+echo " <----- Install good ! -----> "
